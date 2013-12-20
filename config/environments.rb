fd3314048ef2abed517d48df0970acb8031b12f2
omardelarosa <thedelarosa@gmail.com>
2013-12-20 15:29:38 -0500

    added new db config

--- config/environments.rb
+++ config/environments.rb
@@ -9,3 +9,17 @@ configure :development do
   )
 
 end
+
+configure :production do 
+# DATABASE_URL:  HEROKU_POSTGRESQL_SILVER_URL
+
+  ActiveRecord::Base.establish_connection(
+    adapter: "postgresql",
+    host: "ec2-54-221-243-6.compute-1.amazonaws.com",
+    username: "ptirswfcnghmlo",
+    password: "uSuF2hnzqEGw7Lr2bIKgBcKk-t",
+    database: "dfecqktn15hi6s",
+    port: "5432"
+  )
+
+end
